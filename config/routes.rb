848c369168284a5cbd7f1b1c6902825bfc5b25e6
Sri Raj Dutt Badam <srirajdutt@gmail.com>
2011-02-25 02:17:28 +0530

    Create the home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FinalApp::Application.routes.draw do
+  get "home/index"
+
+  get "home/about"
+
+  get "home/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
