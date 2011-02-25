5c043288e40f8889af145d0ad04ecfa3d8448791
Sri Raj Dutt Badam <srirajdutt@gmail.com>
2011-02-25 18:50:47 +0530

    done with basic user pages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FinalApp::Application.initialize!
+
+#AuthLogic
+config.gem "authlogic"
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -11,4 +11,12 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector("title", :content => "Sign up")
   end
+
+  #checking if the links are proper on the pages or not
+  it "should have the rights links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+  end
+
 end
